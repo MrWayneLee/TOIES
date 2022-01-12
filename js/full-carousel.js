475b2751349038bbb0f461f711794e11223a8624
MrWayneLee <1165461458@qq.com>
2022-01-12 14:41:30 +0800

    bug fixd, add responsive

--- js/full-carousel.js
+++ js/full-carousel.js
@@ -1,28 +1,23 @@
 $(document).ready(function(){
-    let ele_carouselMain = $("#carousel-main")
-    var carouselMainHeight = ele_carouselMain.height()
-    var carouselMainWidth = ele_carouselMain.width()
-    
+    var oneFlame
     var carouselMainIndex = 0
+    
+    let ele_carouselMain = $("#carousel-main")
     let ele_carouselMainList = $(".carousel-main-list")
     let ele_carouselMainItem = $(".carousel-main-item")
     let ele_carouselSubList = $(".carousel-sub-list")
     let ele_carouselSubItem = $(".carousel-sub-item")
     
-    for(var index of ele_carouselMainItem) {
-        index.style.height = carouselMainHeight + "px"
-    }
-    for(var index of ele_carouselSubItem) {
-        index.style.width = carouselMainWidth + "px"
-    }
+    var carouselMainHeight = ele_carouselMain.height()
+    var carouselMainWidth = ele_carouselMain.width()
     for(var index of ele_carouselSubList) {
-        console.log($(index));
         $(index).children().eq(0).addClass("active").siblings().removeClass("active")
     }
     ele_carouselMainItem.eq(carouselMainIndex).addClass("active").siblings().removeClass("active")
-    ele_carouselSubList.children("li:nth(0)")
     ele_carouselMainList.after('<i class="bi bi-arrow-up btn-up"></i><i class="bi bi-arrow-down btn-down"></i>')
     ele_carouselSubList.after('<i class="bi bi-arrow-left btn-left"></i><i class="bi bi-arrow-right btn-right"></i>')
+    ele_carouselSubList.css("transform", "translateX(0px)")
+    windowResize()
     
     $(".btn-up").on("click", function() {
         prevPage()
@@ -42,11 +37,36 @@ $(document).ready(function(){
     })
 
     window.onmousewheel = document.onmousewheel = (e) => {
-        if(e.wheelDelta > 0) {
-            prevPage()
-        } else if(e.wheelDelta < 0) {
-            nextPage()
+        if(oneFlame) clearTimeout(oneFlame)
+        oneFlame = setTimeout(function() {
+            if(e.wheelDelta > 0) {          // 上滑
+                prevPage()
+            } else if(e.wheelDelta < 0) {   // 下滑
+                nextPage()
+            }
+        }, 50)
+    }
+
+    window.onresize = () => {
+        windowResize()
+    }
+    
+    function windowResize() {
+        carouselMainHeight = ele_carouselMain.height()
+        carouselMainWidth = ele_carouselMain.width()
+        for(var index of ele_carouselMainItem) {
+            $(index).css("height", carouselMainHeight + "px")
         }
+        for(var index of ele_carouselSubItem) {
+            $(index).css("width", carouselMainWidth + "px")
+        }
+        for(var index of ele_carouselSubList) {
+            var offset = parseInt($(index).css("transform").substring(6).split(",")[4]) / carouselMainWidth
+            var offsetIndex = Math.abs(Math.round(offset))
+            console.log(offsetIndex, carouselMainWidth ,offsetIndex * carouselMainWidth)
+            $(index).css("transform", "translateX(-" + offsetIndex * carouselMainWidth + "px)")
+        }
+        ele_carouselMainList.css({"transform": "translateY(-" + carouselMainIndex * carouselMainHeight + "px)"})
     }
 
     function prevPage() {
@@ -62,20 +82,17 @@ $(document).ready(function(){
     }
 
     function subPrevPage(_this) {
-        if(!_this.siblings("ul").children(".active").prev().length) {
-            return
-        }
-        _this.siblings("ul").css("transform","translateX(-" + (carouselMainWidth + parseInt(_this.siblings("ul").css("transform").substring(6).split(",")[4])) + "px)")
+        if(!_this.siblings("ul").children(".active").prev().length)  return
+        var offset = carouselMainWidth + parseInt(_this.siblings("ul").css("transform").substring(6).split(",")[4])
+        _this.siblings("ul").css("transform", "translateX(" + offset + "px)")
         _this.siblings("ul").children(".active").prev().addClass("active").siblings().removeClass("active")
     }
-
+    
     function subNextPage(_this) {
-        if(!_this.siblings("ul").children(".active").next().length) {
-            return
-        }
-        _this.siblings("ul").css("transform","translateX(-" + (carouselMainWidth - parseInt(_this.siblings("ul").css("transform").substring(6).split(",")[4])) + "px)")
+        if(!_this.siblings("ul").children(".active").next().length)  return
+        var offset = carouselMainWidth - parseInt(_this.siblings("ul").css("transform").substring(6).split(",")[4])
+        _this.siblings("ul").css("transform", "translateX(-" + offset + "px)")
         _this.siblings("ul").children(".active").next().addClass("active").siblings().removeClass("active")
     }
 
 });
-
